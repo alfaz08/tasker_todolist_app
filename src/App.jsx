65113b1389a012550d63bab51ff1b574180ed0be
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-11 07:04:57 +0600

    basic taskboard setup is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import TaskBoard from "./task/TaskBoard"
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
      <Header></Header>
      <Hero></Hero>
+     <TaskBoard></TaskBoard>
      <Footer></Footer>
     </>
   )
